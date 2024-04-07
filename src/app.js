a20b282e8e0ee0b3e6d6832ac0ed36f61915e325
Divyanshu Pathak <divyanshupathak96@gmail.com>
2024-04-07 21:54:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,30 +1,39 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url ==='/') {
-    const chunks = [];
+  if (req.method === 'POST' && req.url === '/') {
+    let body = '';
 
     req.on('data', chunk => {
-      const buf = Buffer.from(chunk);
-      const str = buf.toString();
-      chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value1 = obj.num1;
-      const value2 = obj.num2;
-
-      // Write code here to calculate power of a number
-      // let result = Math.pow(value1, value2);
-      if(value1>1 && value2>0){
-        let result=Math.pow(value1, value2);
-        console.log(result);
-        res.status(200).JSON(result);
-      }else{
-        res.status(400).JSON("The operation cannot be performed");
-      }
-      
+      body += chunk.toString();
     });
-    }
+
+    req.on('end', () => {
+      try {
+        const obj = JSON.parse(body);
+        const value1 = obj.num1;
+        const value2 = obj.num2;
+
+        if (value1 <= 0) {
+          res.writeHead(404, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'The operation cannot be performed' }));
+        } else if (value2 < 0) {
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ error: 'The operation cannot be performed' }));
+        } else {
+          let result = Math.pow(value1, value2);
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ result: result }));
+        }
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Invalid JSON' }));
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
 });
 
 module.exports = server;
-      
