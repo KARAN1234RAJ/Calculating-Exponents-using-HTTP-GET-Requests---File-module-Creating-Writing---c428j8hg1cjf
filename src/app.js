05a1507169e6746af084d3d96f7d0da835734532
KARAN1234RAJ <36782443+KARAN1234RAJ@users.noreply.github.com>
2024-04-07 21:29:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === 'POST' && req.url ==='/') {
     const chunks = [];
 
     req.on('data', chunk => {
@@ -13,6 +13,14 @@ const server = http.createServer((req, res) => {
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
+      // let result = Math.pow(value1, value2);
+      if(value1>1 && value2>0){
+        let result=Math.pow(value1, value2);
+        console.log(result);
+        res.status(200).JSON(result);
+      }else{
+        res.status(400).JSON("The operation cannot be performed");
+      }
       
     });
     }
